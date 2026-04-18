099044a663b2c10ce174b2c2852bed08a52a2798
Yashwanth1129 <ananthayashwanth143@gmail.com>
2026-04-18 18:01:50 -0400

    changes made to main

--- eCommerceSolution.OrdersService/obj/Debug/net10.0/eCommerceSolution.OrdersService.AssemblyInfo.cs
+++ eCommerceSolution.OrdersService/obj/Debug/net10.0/eCommerceSolution.OrdersService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerceSolution.OrdersService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4904eb27a49af58d45b48b4aad973c74a43657")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7075dbb5ef44f3a04bb4c544f76bc7eb5701786")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerceSolution.OrdersService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerceSolution.OrdersService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
